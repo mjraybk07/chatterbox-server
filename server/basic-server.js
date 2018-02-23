9852cc44e0a70d9fa3c92affbd078514d4c2632f
Michael Flis <michaelflis07@gmail.com>
2018-02-23 15:12:19 -0800

    Add server/utils.js and refactored request-handler to require utils exports

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,8 @@
 /* Import node's http module: */
 var http = require('http');
 var handleRequest = require('./request-handler');
+var urlParser = require ('url');
+
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
@@ -22,7 +24,19 @@ var ip = '127.0.0.1';
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(function(request, response) {
+  //console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  
+  var parts = urlParser.parse(request.url);
+  //console.log('parts.....', parts);
+  
+  if ( parts.pathname === '/chatterbox/classes')  {
+    handleRequest(request, response);
+  } else {
+    // TODO: 404
+  }
+  
+});
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,26 +1,29 @@
-var headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'application/json'
-};
-var sendResponse = function ( response, data, statusCode ) {
-  statusCode = statusCode || 200;
-  response.writeHead(statusCode, headers);
-  response.end(JSON.stringify(data));
-};
+// var headers = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10, // Seconds.
+//   'Content-Type': 'application/json'
+// };
 
-var collectData = function(request, callback) {
-  var data = '';
+// var sendResponse = function ( response, data, statusCode ) {
+//   statusCode = statusCode || 200;
+//   response.writeHead(statusCode, headers);
+//   response.end(JSON.stringify(data));
+// };
+
+// var collectData = function(request, callback) {
+//   var data = '';
   
-  request.on('data', function(chunk) {
-    data += chunk;
-  });
-  request.on ('end', function() {
-    callback( JSON.parse(data) );
-  });
-};
+//   request.on('data', function(chunk) {
+//     data += chunk;
+//   });
+//   request.on ('end', function() {
+//     callback( JSON.parse(data) );
+//   });
+// };
+
+var utils = require ('./utils')
 
 var objectId = 1;
 
@@ -34,17 +37,17 @@ var messages = [
 
 var actions = {
   'GET': function(request, response) {
-    sendResponse(response, {results: messages}); 
+    utils.sendResponse(response, {results: messages}); 
   },
   'POST': function(request, response) {
-    collectData(request, function (message) {
+    utils.collectData(request, function (message) {
     messages.push(message);
     message.objectId = ++objectId;
     sendResponse(response, {objectId: 1});
     });
   },
   'OPTIONS': function(request, response) {
-    sendResponse(response, null);
+    utils.sendResponse(response, null);
   },  
   
 };
@@ -52,14 +55,14 @@ var actions = {
 
 module.exports = function(request, response) {
 
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  //console.log('Serving request type ' + request.method + ' for url ' + request.url);
   
   var action = actions[request.method];
   
   if ( action ) {
     action(request, response)
   } else {
-    sendReponse(response, "Not Found", 404);
+    utils.sendReponse(response, "Not Found", 404);
   }
   
   // if ( action === 'GET' ) {
